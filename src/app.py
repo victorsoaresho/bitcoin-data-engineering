010a6002c336b5b9f4f489b511bb0f5c9cdf9875
Victor <victorsoaresho@gmail.com>
2025-01-06 19:42:47 -0300

    feat: Up the API code

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from controllers.controllerCoinGecko import coingecko_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(coingecko_blueprint)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
